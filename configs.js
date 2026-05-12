873dca16830b7f53fbf319e535c61e82ba67f9e4
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-05-12 13:09:53 +0330

    implement Sequelize configurations

--- configs.js
+++ configs.js
@@ -1,25 +1,28 @@
-require('dotenv').config(); 
 module.exports = {
   db: {
-    user:process.env.DB_USER,
-    password : procces.env.DB_PASSWORD,
-    name:procces.env.BD_NAME,
-    host:procces.env.DB_HOST,
-    port:process.env.DB_PORT,
-    dialect:process.env.DB_DIALECT,
-    poolsize:process.env.DB_POOL_SIZE || 30,
-
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    poolSize: process.env.DB_POOL_SIZE || 30,
   },
+
   port: parseInt(process.env.PORT) || 4000,
+
   auth: {
     accessTokenSecretKey: process.env.ACCESS_TOKEN_SECRET_KEY,
     refreshTokenSecretKey: process.env.REFRESH_TOKEN_SECRET_KEY,
     accessTokenExpiresInSeconds: process.env.ACCESS_TOKEN_EXPIRES_IN_SECONDS,
     refreshTokenExpiresInSeconds: process.env.REFRESH_TOKEN_EXPIRES_IN_SECONDS,
-    google:{}
-  },
-  redis:{},
-  domain:proccess.env.DOMAIN,
-  isProduction: proccess.env.NODE_ENV=== "production"
 
+    google: {},
+  },
+
+  redis: {},
+
+  domain: process.env.DOMAIN,
+
+  isProduction: process.env.NODE_ENV === "production",
 };
