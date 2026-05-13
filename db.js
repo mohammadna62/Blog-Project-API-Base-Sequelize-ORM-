2155b28c751fa66514a107f46a84e6c9a94a1fae
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-05-13 13:52:29 +0330

    feat: implement model associations

--- db.js
+++ db.js
@@ -1,17 +1,38 @@
 const { Sequelize } = require("sequelize");
+const configs = require("./configs");
 
-const configs = require("./configs")
-
+const User = require("./models/User");
+const Tag = require("./models/Tag");
+const TagsArticles = require("./models/TagsArticles");
+const Article = require("./models/Articles");
 
 const db = new Sequelize({
-    host:configs.db.host,
-    port:configs.db.port,
-    username:configs.db.user,
-    password:configs.db.password,
-    database:configs.db.name,
-    dialect:configs.db.dialect,
-    logging: configs.isProduction ? false : console.log,
-    
-})
+  host: configs.db.host,
+  port: configs.db.port,
+  username: configs.db.user,
+  password: configs.db.password,
+  database: configs.db.name,
+  dialect: configs.db.dialect,
+  logging: configs.isProduction ? false : console.log,
+});
 
-module.exports = db
+User.hasMany(Article, {
+  foreingKey: "author_id",
+  onDelete: "CASCADE",
+});
+
+Article.belongsTo(User, { foreingKey: "author_id", as: "author" });
+
+Article.belongsToMany(Tag, {
+  through: TagsArticles,
+  onDelete: "CASCADE",
+  foreingKey: "article_id",
+});
+
+Tag.belongsToMany(Article, {
+  through: TagsArticles,
+  onDelete: "CASCADE",
+  foreingKey: "tag_id",
+});
+
+module.exports = db;
