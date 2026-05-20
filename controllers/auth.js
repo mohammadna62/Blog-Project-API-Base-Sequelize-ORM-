4b5f881b64e406c7bcb0760f50f3e71fba3b29ba
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-05-20 11:33:44 +0330

    feat: implement login with Passport package

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,8 @@
-const {User} = require("./../db")//! Because used of Sequelize
-const bcrypt = require("bcrypt")
+const { User } = require("./../db"); //! Because used of Sequelize
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const configs = require("../configs");
-const  redis  = require("./../redis");
+const redis = require("./../redis");
 
 exports.register = async (req, res, next) => {
   try {
@@ -31,8 +31,13 @@ exports.register = async (req, res, next) => {
       },
     );
     const hashedRefreshToken = await bcrypt.hash(refreshToken, 12);
-    await redis.set(`refreshToken:${user.id}`,hashedRefreshToken,"EX",configs.auth.refreshTokenExpiresInSeconds)
-     return res.status(201).json({
+    await redis.set(
+      `refreshToken:${user.id}`,
+      hashedRefreshToken,
+      "EX",
+      configs.auth.refreshTokenExpiresInSeconds,
+    );
+    return res.status(201).json({
       accessToken,
       refreshToken,
     });
@@ -40,7 +45,36 @@ exports.register = async (req, res, next) => {
     next(err);
   }
 };
-exports.login= async(req , res , next)=>{
-  console.log("login");
-  
-}
+exports.login = async (req, res, next) => {
+  try {
+    const user = req.user;
+
+    const accessToken = jwt.sign(
+      { id: user.id, role: user.role },
+      configs.auth.accessTokenSecretKey,
+      {
+        expiresIn: configs.auth.accessTokenExpiresInSeconds + "s",
+      },
+    );
+
+    const refreshToken = jwt.sign(
+      { id: user.id },
+      configs.auth.refreshTokenSecretKey,
+      {
+        expiresIn: configs.auth.refreshTokenExpiresInSeconds + "s",
+      },
+    );
+
+    const hashedRefreshToken = await bcrypt.hash(refreshToken, 12);
+    await redis.set(
+      `refreshToken:${user.id}`,
+      hashedRefreshToken,
+      "EX",
+      configs.auth.refreshTokenExpiresInSeconds,
+    );
+
+    return res.status(200).json({ accessToken, refreshToken });
+  } catch (err) {
+    next(err);
+  }
+};
--- strategies/localStrategy.js
+++ strategies/localStrategy.js
@@ -1,5 +1,18 @@
-const localStrategy = require('passport-local')
+const { User } = require('../db');
+const bcrypt = require("bcrypt")
+const localStrategy = require('passport-local').Strategy
+
 module.exports = new localStrategy(async(username , password , done)=>{
-   console.log(username , password);
-   
+
+   const user = await User.findOne({
+      where:{
+         username
+      },
+      raw: true, //* For resave Raw Data
+   })
+   if(!user)return done(null,false)
+      const isPasswordValid = await bcrypt.compare(password,user.password)
+   if(!isPasswordValid)return done(null,false)
+      return done(null,user) //* set this user to req.user = user
+
 })
