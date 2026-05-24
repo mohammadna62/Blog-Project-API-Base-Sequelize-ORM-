9b22f76dee4ecf48f3504f2a6324d84c67ee085b
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-05-24 11:13:12 +0330

    feat: create new tags in database

--- controllers/article.js
+++ controllers/article.js
@@ -1,5 +1,44 @@
+const { default: slugify } = require("slugify");
+const { Article, Tag } = require("./../db");
+
 exports.create = async (req, res, next) => {
-  console.log("ok");
-  
-  return res.json("ok");
+  try {
+    let { title, content, tags } = req.body; // frontend -> ["frontend"]
+    const slug = slugify(title, { lower: true });
+    const authorId = req.user.id;
+
+    tags = Array.isArray(tags) ? tags : [tags];//* Convert to an array to have the same type of data (String or Array)
+
+    tags = tags.map((tag) =>
+      Tag.findOrCreate({ where: { title: tag.trim() } })
+    );
+    tags = await Promise.all(tags);
+
+    // const fileBuffer = req.file.buffer;
+    // const coverPath = `/images/covers/${Date.now()}${req.file.originalname}`;
+
+    // sharp(fileBuffer)
+    //   .png({
+    //     quality: 60,
+    //   })
+    //   .toFile(`./public${coverPath}`);
+
+    // const article = await Article.create({
+    //   title,
+    //   content,
+    //   slug,
+    //   author_id,
+    //   cover: coverPath,
+    // });
+
+    // tags.forEach(async (tag) => {
+    //   await Article.addTag(article.id, Number(tag));
+    // });
+
+    // req.flash("success", "مقاله مورد نظر با موفقیت ایجاد شد");
+    // return res.redirect("/p-admin/create-article");
+    return res.json("ok")
+  } catch (err) {
+    next(err);
+  }
 };
--- routes/articles.js
+++ routes/articles.js
@@ -1,17 +1,49 @@
 const express = require("express");
 const controller = require("./../controllers/article");
-const createArticleSchema = require("./../validators/createArticle");
-const validator = require("./../middlewares/validate");
+const validate = require("./../middlewares/validate");
+const createArticleSchema = require("../validators/createArticle");
 const passport = require("passport");
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.resolve(__dirname, "../public/images/covers"));
+  },
+
+  filename: (req, file, cb) => {
+    const fileName =
+      `${file.originalname}-${Date.now()}` + path.extname(file.originalname);
+    cb(null, fileName);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const validFileTypes = /jpeg|jpg|png/;
+  const mimeType = validFileTypes.test(file.mimetype);
+  const extName = validFileTypes.test(path.extname(file.originalname));
+
+  if (mimeType && extName) {
+    return cb(null, true);
+  }
+
+  return cb(new Error("File type not supported !"));
+};
+
+const uploader = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 3 * 1024 * 1024 }, // 3MB
+});
 
 const router = express.Router();
 
-router
-  .route("/")
-  .post(
-    validator(createArticleSchema),
-    passport.authenticate("accessToken",{session:false}),
-    controller.create,
-  );
+router.route("/").post(
+  passport.authenticate("accessToken", { session: false }),
+  uploader.single("cover"),
+  validate(createArticleSchema),
+
+  controller.create
+);
 
 module.exports = router;
